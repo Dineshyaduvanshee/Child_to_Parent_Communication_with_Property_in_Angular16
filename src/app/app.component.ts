c96c1c89e942a1c274247d8322ef8462078b4bcc
Dinesh yadav <dineshyadav74950@gmail.com>
2024-01-17 17:01:27 +0530

    In this code i am using *ngIf ,else,then,thenblock and else block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,34 @@
-import { Component } from '@angular/core';
+// import { NgIfContext } from '@angular/common';
+// import { Component, TemplateRef } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+
+
+//   title = 'Child_to_Parent_Communication_with_Property_in_Angular16';
+//   cdata : any;
+//   sdata: any;
+//   isValid : boolean = true;
+
+
+//   getData(val : any){
+//     this.cdata = val;
+//   }
+//   getStudentData(val : any){
+//     this.sdata = val;
+//   }
+//   changeValue(valid : boolean){
+//     this.isValid = valid;
+//   }
+// }
+
+
+import { NgIfContext } from '@angular/common';
+import { Component, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +36,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   title = 'Child_to_Parent_Communication_with_Property_in_Angular16';
-  cdata : any;
+  cdata: any;
   sdata: any;
-  getData(val : any){
+  isValid: boolean = true;
+  thenblock: TemplateRef<NgIfContext> | null = null;
+  elseblock: TemplateRef<NgIfContext> | null = null;
+
+  getData(val: any) {
     this.cdata = val;
   }
-  getStudentData(val : any){
+
+  getStudentData(val: any) {
     this.sdata = val;
+  }
+
+  changeValue(valid: boolean) {
+    this.isValid = valid;
   }
 }
